336c3256af538c52d6596ac594605d5a7098f972
hammoud442 <mattrash@dev-any.com>
2019-05-13 18:08:50 +0300

    final commit

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,47 @@
-$('.owl-carousel').owlCarousel({    loop:true,    margin:10,    items:4,    autoplay:true,    responsiveClass:true,    responsive:{        0:{            items:1,            nav:true        },        600:{            items:3,            nav:false        },        1000:{            items:5,            nav:true,            loop:false        }    }});function myMap() {    var mapProp= {        center:new google.maps.LatLng(51.508742,-0.120850),        zoom:5,    };    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);    var map2 = new google.maps.Map(document.getElementById("googleMap2"),mapProp);};$(document).ready(function () {    $('#btnLogin').click(function () {        document.getElementById("message").style.visibility = "visible";    });});$(document).ready(function () {    $('#close').click(function () {        document.getElementById("message").style.visibility = "hidden";    });});
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    items:4,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+});
+
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var map2 = new google.maps.Map(document.getElementById("googleMap2"),mapProp);
+};
+
+$(document).ready(function () {
+    $('#btnLogin').click(function (e) {
+        e.preventDefault();
+        $('#message').fadeIn();
+    });
+
+});
+$(document).ready(function () {
+    $('#close').click(function (e) {
+        e.preventDefault();
+
+        $('#message').fadeOut();
+    });
+
+});
