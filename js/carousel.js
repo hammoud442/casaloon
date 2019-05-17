c5b22ddf259c170bc979f7f8c3d8ff40bcec969b
hammoud442 <mattrash@dev-any.com>
2019-05-17 03:16:15 +0300

    final commit

--- js/carousel.js
+++ js/carousel.js
@@ -7,16 +7,12 @@ $('.owl-carousel').owlCarousel({
     responsive:{
         0:{
             items:1,
-            nav:true
         },
         600:{
             items:3,
-            nav:false
         },
         1000:{
             items:5,
-            nav:true,
-            loop:false
         }
     }
 });
